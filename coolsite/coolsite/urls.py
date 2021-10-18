dccba5fd26de995bf6db8bbbd1cd590ace4e0b50
Arkady <aaskorohodov@gmail.com>
2021-10-18 19:28:00 +0500

    Создана модель women, выполнена миграция

--- coolsite/coolsite/urls.py
+++ coolsite/coolsite/urls.py
@@ -13,8 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from coolsite import settings
 from women.views import *
 
 
@@ -22,3 +25,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('women.urls'))  # include включает все маршруты из указанного каталога и файла (women.urls)
 ]
+
+if settings.DEBUG:  # в режиме отладки, когда DEBUG == True
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Women(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)  # blank=True = может быть пустым
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")  # описывает каталог, куда класть фото (папки)
+    time_create = models.DateTimeField(auto_now_add=True)  # auto_now_add не меняется
+    time_update = models.DateTimeField(auto_now=True)  # auto_now меняется
+    is_published = models.BooleanField(default=True)
