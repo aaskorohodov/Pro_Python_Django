055224674d770ef81dbb48438b974252064e96c8
Arkady <aaskorohodov@gmail.com>
2021-10-23 15:35:05 +0500

    Расширены шаблоны

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
 
     path('cats/<int:catid>/', categories),  # отлавливаем любое число, пакуем в переменную catid, передаем в views
     re_path(r'^archive/(?P<year>[0-9]{4})/', archive),  # re_path умеет работать с регулярными выражениями
-    path('about/', about, name='about')
+    path('about/', about, name='about'),
+    path('addpage/', addpage, name='add_page'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+    path('post/<int:post_id>/', show_post, name='post'),
 ]
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -2,12 +2,25 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import *
 
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+]
 
 
 def index(request):
     posts = Women.objects.all()  # берем все записи модели, ниже передаем их в шаблон
-    return render(request, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
+
+    '''Это словарь, чтобы передавать параметры в шаблоны. Можно прописать параметры ниже в рендер, но так красивше.
+    Ниже нужно передать этот словарь context в специальную переменную context (словарь можно назвать иначе,
+    а переменная именно context'''
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Главная страница'
+    }
+    return render(request, 'women/index.html', context=context)
 
 
 def about(request):
@@ -22,3 +35,19 @@ def categories(request, catid):  # catid рандомное имя, отлавл
 
 def archive(request, year):  # year отлавливается в urls.py
     return HttpResponse(f"<h1>Архив по годам</h1>{year}</p>")
+
+
+def addpage(request):
+    return HttpResponse("Добавление статьи")
+
+
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+
+def login(request):
+    return HttpResponse("Авторизация")
+
+
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
